e5b72c516e06ac8994400bbb7c4e6db21c43bbb3
Marcel Hernandez <e6990620@gmail.com>
2024-11-13 14:01:41 -0300

    replace python-bitcoinlib for bitcoin-utils

--- examples/p2wpkh.py
+++ examples/p2wpkh.py
@@ -1,95 +1,60 @@
-import sys
-
-import hashlib
-
-from bitcoin import SelectParams
-from bitcoin.core import b2x, b2lx, lx, COIN, COutPoint, CTxOut, CTxIn, CTxInWitness, CTxWitness, CScriptWitness, CMutableTransaction, Hash160
-from bitcoin.core.script import CScript, OP_0, SignatureHash, SIGHASH_ALL, SIGVERSION_WITNESS_V0
-from bitcoin.wallet import CBitcoinSecret, P2WPKHBitcoinAddress
-from bitcoin.rpc import Proxy
+from bitcoinutils.setup import setup
+from bitcoinutils.utils import to_satoshis
+from bitcoinutils.transactions import Transaction, TxInput, TxOutput, TxWitnessInput
+from bitcoinutils.keys import P2pkhAddress, PrivateKey
+from bitcoinutils.script import Script
 
 
 def spend_p2wpkh():
-    SelectParams("regtest")
-    connection = Proxy()
+    # always remember to setup the network
+    setup("testnet")
 
-    if connection._call("getblockchaininfo")["chain"] != "regtest":
-        sys.stderr.write("This example is intended for regtest only.\n")
-        sys.exit(1)
+    # the key that corresponds to the P2WPKH address
+    priv = PrivateKey("cVdte9ei2xsVjmZSPtyucG43YZgNkmKTqhwiUA8M4Fc3LdPJxPmZ")
 
+    pub = priv.get_public_key()
 
-    # Create the (in)famous correct brainwallet secret key.
-    h = hashlib.sha256(b'correct horse battery staple').digest()
-    seckey = CBitcoinSecret.from_secret_bytes(h)
+    fromAddress = pub.get_segwit_address()
+    print(fromAddress.to_string())
 
-    # Create an address from that private key.
-    public_key = seckey.pub
-    scriptPubKey = CScript([OP_0, Hash160(public_key)])
-    address = P2WPKHBitcoinAddress.from_scriptPubKey(scriptPubKey)
+    # amount is needed to sign the segwit input
+    fromAddressAmount = to_satoshis(0.01)
 
-    # Give the private key to bitcoind (for ismine, listunspent, etc).
-    connection._call("importprivkey", str(seckey))
+    # UTXO of fromAddress
+    txid = "13d2d30eca974e8fa5da11b9608fa36905a22215e8df895e767fc903889367ff"
+    vout = 0
 
-    # Check if there's any funds available.
-    unspentness = lambda: connection._call("listunspent", 6, 9999, [str(address)], True, {"minimumAmount": 1.0})
-    unspents = unspentness()
-    while len(unspents) == 0:
-        # mine some funds into the address
-        connection._call("generatetoaddress", 110, str(address))
-        unspents = unspentness()
+    toAddress = P2pkhAddress("mrrKUpJnAjvQntPgz2Z4kkyr1gbtHmQv28")
 
-    # Choose the first UTXO, let's spend it!
-    unspent_utxo_details = unspents[0]
-    txid = unspent_utxo_details["txid"]
-    vout = unspent_utxo_details["vout"]
-    amount = int(float(unspent_utxo_details["amount"]) * COIN)
+    # create transaction input from tx id of UTXO
+    txin = TxInput(txid, vout)
 
-    # Calculate an amount for the upcoming new UTXO. Set a high fee to bypass
-    # bitcoind minfee setting.
-    amount_less_fee = int(amount - (0.01 * COIN))
+    # in segwit the signature message should commit to the script code
+    # that corresponds to the segwit transaction output
+    # the script code (template) required for signing for p2wpkh is the
+    # same as p2pkh
+    script_code = Script(
+        ["OP_DUP", "OP_HASH160", pub.to_hash160(), "OP_EQUALVERIFY", "OP_CHECKSIG"]
+    )
 
-    # Create a destination to send the coins.
-    destination_address = connection._call("getnewaddress", "python-bitcoinlib-example", "bech32")
-    destination_address = P2WPKHBitcoinAddress(destination_address)
-    target_scriptPubKey = destination_address.to_scriptPubKey()
+    # create transaction output
+    txOut = TxOutput(to_satoshis(0.009), toAddress.to_script_pub_key())
 
-    # Create the unsigned transaction.
-    txin = CTxIn(COutPoint(lx(txid), vout))
-    txout = CTxOut(amount_less_fee, target_scriptPubKey)
-    tx = CMutableTransaction([txin], [txout])
+    # create transaction without change output - if at least a single input is
+    # segwit we need to set has_segwit=True
+    tx = Transaction([txin], [txOut], has_segwit=True)
 
-    # Specify which transaction input is going to be signed for.
-    txin_index = 0
+    print("\nRaw transaction:\n" + tx.serialize())
 
-    # When signing a P2WPKH transaction, use an "implicit" script that isn't
-    # specified in the scriptPubKey or the witness.
-    redeem_script = address.to_redeemScript()
+    sig = priv.sign_segwit_input(tx, 0, script_code, fromAddressAmount)
 
-    # Calculate the signature hash for the transaction. This is then signed by the
-    # private key that controls the UTXO being spent here at this txin_index.
-    sighash = SignatureHash(redeem_script, tx, txin_index, SIGHASH_ALL, amount=amount, sigversion=SIGVERSION_WITNESS_V0)
-    signature = seckey.sign(sighash) + bytes([SIGHASH_ALL])
+    # note that TxWitnessInput gets a list of witness items (not script opcodes)
+    tx.witnesses.append(TxWitnessInput([sig, pub.to_hex()]))
 
-    # Construct a witness for this transaction input. The public key is given in
-    # the witness so that the appropriate redeem_script can be calculated by
-    # anyone. The original scriptPubKey had only the Hash160 hash of the public
-    # key, not the public key itself, and the redeem script can be entirely
-    # re-constructed  if given just the public key. So the public key is added to
-    # the witness. This is P2WPKH in bip141.
-    witness = [signature, public_key]
+    # print raw signed transaction ready to be broadcasted
+    print("\nRaw signed transaction:\n" + tx.serialize())
+    print("\nTxId:", tx.get_txid())
 
-    # Aggregate all of the witnesses together, and then assign them to the
-    # transaction object.
-    ctxinwitnesses = [CTxInWitness(CScriptWitness(witness))]
-    tx.wit = CTxWitness(ctxinwitnesses)
-
-    # Broadcast the transaction to the regtest network.
-    spend_txid = connection.sendrawtransaction(tx)
-
-    # Done! Print the transaction to standard output. Show the transaction
-    # serialization in hex (instead of bytes), and render the txid.
-    print("serialized transaction: {}".format(b2x(tx.serialize())))
-    print("txid: {}".format(b2lx(spend_txid)))
 
 if __name__ == '__main__':
     spend_p2wpkh()
