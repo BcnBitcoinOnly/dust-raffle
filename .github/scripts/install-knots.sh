57558d7eb148ca02c8eeb75b9c823abdca02ecc8
Marcel Hernandez <e6990620@gmail.com>
2026-03-28 01:40:06 +0100

    updates

--- .github/scripts/install-knots.sh
+++ .github/scripts/install-knots.sh
@@ -6,7 +6,7 @@ DIRNAME="bitcoin-${KNOTS_VERSION}"
 FILENAME="${DIRNAME}-x86_64-linux-gnu.tar.gz"
 
 cd "${HOME}"
-wget -q "https://bitcoinknots.org/files/27.x/${KNOTS_VERSION}/bitcoin-${KNOTS_VERSION}-x86_64-linux-gnu.tar.gz"
+wget -q "https://bitcoinknots.org/files/29.x/${KNOTS_VERSION}/bitcoin-${KNOTS_VERSION}-x86_64-linux-gnu.tar.gz"
 tar -xf "${FILENAME}"
 sudo mv "${DIRNAME}"/bin/* "/usr/local/bin"
 rm -rf "${FILENAME}" "${DIRNAME}"
